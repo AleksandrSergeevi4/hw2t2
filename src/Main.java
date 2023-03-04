91eef584a7be6534943d926ad230d09f2d34490b
Alex.Sn <alex7777@nxt.ru>
2023-03-04 20:44:57 +0300

    change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
-        // Счет клинета
-        int clientAccount;
-        clientAccount = 100;
-
-        // Пополнение счета
-        int replenishment;
-            replenishment = 1350;
-
+        int clientAccount = 100;
+        int replenishment = 1350;
         System.out.println("Пополнение счета на:" + (replenishment) + "руб.");
-
-        // 1 Балл за каждые 100 руб
-        int bonus;
+        int bonus; // 1 Балл за каждые 100 руб
         if (replenishment >= 1000) {
             bonus = replenishment / 100;
             System.out.println("Ваш бонус:" + bonus + "руб.");
         } else {
             bonus = 0;
         }
-        // Результат
-        int results;
-        results = clientAccount + replenishment + bonus;
-
+        int results = clientAccount + replenishment + bonus;
         System.out.println("Ваш счет:" + results + "руб.");
     }
 
